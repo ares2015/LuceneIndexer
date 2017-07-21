f7de48665ad006b88dce0cd51b54f9a2da47e2f9
Oliver Eder <eder@essential-data.sk>
2017-07-21 08:57:54 +0200

    Multi threading index writing.

---
+++ src/main/java/com/luceneIndexer/indexer/LuceneIndexWriter.java
@@ -0,0 +1,58 @@
+package com.luceneIndexer.indexer;
+
+import org.apache.lucene.analysis.standard.StandardAnalyzer;
+import org.apache.lucene.analysis.util.CharArraySet;
+import org.apache.lucene.document.Document;
+import org.apache.lucene.index.IndexWriter;
+import org.apache.lucene.index.IndexWriterConfig;
+import org.apache.lucene.store.Directory;
+import org.apache.lucene.store.FSDirectory;
+import org.apache.lucene.util.Version;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+
+/**
+ * Created by Oliver on 7/21/2017.
+ */
+public class LuceneIndexWriter implements Runnable {
+
+    Directory indexDir = FSDirectory.open(new File("C:\\Users\\Oliver\\Documents\\NlpTrainingData\\Lucene\\Index"));
+
+    IndexWriterConfig config = new IndexWriterConfig(Version.LUCENE_4_10_0, new StandardAnalyzer(CharArraySet.EMPTY_SET));
+
+    private List<Document> docList;
+
+    public LuceneIndexWriter(List<Document> docList) throws IOException {
+        this.docList = docList;
+    }
+
+    @Override
+    public void run() {
+        write();
+    }
+
+    public void write() {
+        try {
+            IndexWriter writer = new org.apache.lucene.index.IndexWriter(indexDir, config);
+            int count = 0;
+            int nrWrittenSentences = 0;
+            for (Document d : docList) {
+                System.out.println("Writing into index: " + d.getField("sentence"));
+                writer.addDocument(d);
+                nrWrittenSentences++;
+                System.out.println("Number of written sentences: " + nrWrittenSentences);
+                // make sure we get 2 segments
+                if (++count % 5 == 0) {
+                    writer.commit();
+                }
+            }
+            writer.commit();
+            writer.close();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+}
--- src/main/java/com/luceneIndexer/indexer/LuceneIndexer.java
+++ src/main/java/com/luceneIndexer/indexer/LuceneIndexer.java
@@ -5,7 +5,6 @@ import org.apache.lucene.analysis.util.CharArraySet;
 import org.apache.lucene.document.Document;
 import org.apache.lucene.document.Field;
 import org.apache.lucene.document.TextField;
-import org.apache.lucene.index.IndexWriter;
 import org.apache.lucene.index.IndexWriterConfig;
 import org.apache.lucene.store.Directory;
 import org.apache.lucene.store.FSDirectory;
@@ -14,6 +13,9 @@ import org.apache.lucene.util.Version;
 import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
 /**
  * Created by Oliver on 7/17/2017.
@@ -27,7 +29,7 @@ public class LuceneIndexer {
         int numberOfSentences = 0;
         Directory indexDir = FSDirectory.open(new File("C:\\Users\\Oliver\\Documents\\NlpTrainingData\\Lucene\\Index"));
         IndexWriterConfig config = new IndexWriterConfig(Version.LUCENE_4_10_0, new StandardAnalyzer(CharArraySet.EMPTY_SET));
-        config.setOpenMode(IndexWriterConfig.OpenMode.APPEND);
+//        config.setOpenMode(IndexWriterConfig.OpenMode.APPEND);
 
         List<Document> docList = new ArrayList<Document>();
         BufferedReader br = null;
@@ -62,25 +64,33 @@ public class LuceneIndexer {
         } catch (final IOException e) {
             e.printStackTrace();
         }
-        try {
-            IndexWriter writer = new IndexWriter(indexDir, config);
-            int count = 0;
-            int nrWrittenSentences = 0;
-            for (Document d : docList) {
-                System.out.println("Writing into index: " + d.getField("sentence"));
-                writer.addDocument(d);
-                nrWrittenSentences++;
-                System.out.println("Number of written sentences: " + nrWrittenSentences);
-                // make sure we get 2 segments
-                if (++count % 5 == 0) {
-                    writer.commit();
-                }
-            }
-            writer.commit();
-            writer.close();
-        } catch (Exception e) {
-            throw new RuntimeException(e);
+
+        int sublistSize = docList.size() / 4;
+
+        List<Document> sublist1 = docList.subList(0, sublistSize);
+        List<Document> sublist2 = docList.subList(sublistSize, sublistSize * 2);
+        List<Document> sublist3 = docList.subList(sublistSize * 2, sublistSize * 3);
+        List<Document> sublist4 = docList.subList(sublistSize * 3, sublistSize * 4);
+
+        ExecutorService executor = Executors.newFixedThreadPool(4);
+
+        Runnable writer1 = new LuceneIndexWriter(sublist1);
+        Runnable writer2 = new LuceneIndexWriter(sublist2);
+        Runnable writer3 = new LuceneIndexWriter(sublist3);
+        Runnable writer4 = new LuceneIndexWriter(sublist4);
+
+        Future<?> future1 = executor.submit(writer1);
+        Future<?> future2 = executor.submit(writer2);
+        Future<?> future3 = executor.submit(writer3);
+        Future<?> future4 = executor.submit(writer4);
+
+
+        boolean areDataWritten = false;
+        while (!areDataWritten) {
+            areDataWritten = !future1.isDone() && !future2.isDone() && !future3.isDone() && !future4.isDone();
         }
+        executor.shutdown();
+
         long stopTime = System.currentTimeMillis();
         long elapsedTime = stopTime - startTime;
         System.out.println(numberOfSentences + " sentences indexed in " + (elapsedTime / 1000) / 60 + " minutes and "
